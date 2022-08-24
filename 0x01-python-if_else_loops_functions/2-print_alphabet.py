7a2aee9e3014936677a8c08f79d3462f2721e737
Chiloane-lt <105154547+Chiloane-lt@users.noreply.github.com>
2022-08-24 05:24:02 +0200

    Update 2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,7 +1,9 @@
+
 #!/usr/bin/python3
 for i in range (97, 123):
-    print(f"{i:c}", end = "")
+    print("{0:c}".format(i),end = "")
 
 # An alternative id to use the method below:
+# print(f"{i:c}", end = "")
 # for i in range (97,123):
 #    print(chr(i), end = "")
