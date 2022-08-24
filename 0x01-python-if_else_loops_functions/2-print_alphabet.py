bbdb77b89eea287c9479f1f4e27371f5c00e56f1
Chiloane-lt <chiloane.lt@hotmail.com>
2022-08-24 10:34:28 +0000

    Undo last commits

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-for i in range (97, 123):
-    print("{}".format(chr(i)),end = "")
+for i in range(97, 123):
+    print(f"{i:c}", end="")
 
+# print("{}".format(chr(i)),end = "")
 # print("{0:c}".format(i),end = "")
 # print(f"{i:c}", end = "")
 # for i in range (97,123):
