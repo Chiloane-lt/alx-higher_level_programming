f26fe2cee293ccaf75640589d5d69d3e7e922047
Chiloane-lt <chiloane.lt@hotmail.com>
2022-08-29 10:49:07 +0000

    Fixed print

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(97, 123):
-    print("{0:c}".format(i),end = "")
+    print("{:c}".format(i),end = "")
 
 # print("{}".format(chr(i)),end = "")
 # print("{0:c}".format(i),end = "")
