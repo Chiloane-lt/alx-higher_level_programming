8fd2c6917efdc548322dfd161d77a4bc193b99e4
Chiloane-lt <chiloane.lt@hotmail.com>
2022-08-30 00:22:43 +0000

    used comprehension

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    nulist = []
+#    nulist = []
 
-    for i in my_list:
-        if (my_list[i] % 2) == 0:
-            nulist.append(True)
-        else:
-            nulist.append(False)
+#    for i in my_list:
+#        if (my_list[i] % 2) == 0:
+#            nulist.append(True)
+#        else:
+#            nulist.append(False)
+#    return nulist
+
+    nulist = [True if my_list[i] % 2 == 0 else False for i in my_list]
     return nulist
