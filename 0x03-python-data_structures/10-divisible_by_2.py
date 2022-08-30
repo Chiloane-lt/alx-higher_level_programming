e9e273e2657e33ec516e70cc9a25684dc6f51507
Chiloane-lt <chiloane.lt@hotmail.com>
2022-08-30 00:23:51 +0000

    Undo last commit

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-#    nulist = []
+    nulist = []
 
-#    for i in my_list:
-#        if (my_list[i] % 2) == 0:
-#            nulist.append(True)
-#        else:
-#            nulist.append(False)
-#    return nulist
-
-    nulist = [True if my_list[i] % 2 == 0 else False for i in my_list]
+    for i in my_list:
+        if (my_list[i] % 2) == 0:
+            nulist.append(True)
+        else:
+            nulist.append(False)
     return nulist
+
