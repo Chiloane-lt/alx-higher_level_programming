a873ec1540c9af8e5c2c0c7a34c80d19cceb099d
Chiloane-lt <chiloane.lt@hotmail.com>
2022-08-31 23:47:37 +0000

    Add guard statement

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-import sys
+if __name__ == "__main__":
+    import sys
 
-list_args = list(sys.argv)
-num_args = len(list_args)
-num_input = num_args - 1
+    list_args = list(sys.argv)
+    num_args = len(list_args)
+    num_input = num_args - 1
 
-if num_args < 2:
-    print("{} arguments.".format(num_input))
-elif num_args == 2:
-    print("{} argument:".format(num_input))
-else:
-    print("{} arguments:".format(num_input))
+    if num_args < 2:
+        print("{} arguments.".format(num_input))
+    elif num_args == 2:
+        print("{} argument:".format(num_input))
+    else:
+        print("{} arguments:".format(num_input))
 
-for i in range(1, num_args):
-    print("{}: {}".format(i, list_args[i]))
+    for i in range(1, num_args):
+        print("{}: {}".format(i, list_args[i]))
